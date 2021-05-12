458a14d5c17a4d31b895ca1ea4bd50cb5fe9aa73
Thanasis Sergouniotis <athanasios.sergouniotis@intrasoft-intl.com>
2021-05-12 11:08:14 +0300

    new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { ItemComponent } from './item/item/item.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'dashboard', component: DashboardComponent},
+  {path:'item', component: ItemComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
